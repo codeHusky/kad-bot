90f616db0f21b7463ada137642fc9e12cf0fa62d
SharkFin <sharkfinpro@hotmail.com>
2019-01-02 17:38:39 -0800

    fix

--- source/commands/index.js
+++ source/commands/index.js
@@ -32,7 +32,7 @@ module.exports = {
             args: ''
         },
         login: {
-            enabled: false,
+            enabled: true,
             description: 'Connect your KA account',
             type: 'general',
             command: 'library/general/login.js',
@@ -81,49 +81,49 @@ module.exports = {
             args: ['username']
         },
         play: {
-            enabled: false,
+            enabled: true,
             description: 'Plays a song from YT',
             type: 'music',
             command: 'library/music/play.js',
             args: ['url']
         },
         yt: {
-            enabled: false,
+            enabled: true,
             description: 'Searches songs to play on YT',
             type: 'music',
             command: 'library/music/yt.js',
             args: ['query']
         },
         skip: {
-            enabled: false,
+            enabled: true,
             description: 'Skips current song',
             type: 'music',
             command: 'library/music/skip.js',
             args: ''
         },
         pause: {
-            enabled: false,
+            enabled: true,
             description: 'Pauses current song',
             type: 'music',
             command: 'library/music/pause.js',
             args: ''
         },
         queue: {
-            enabled: false,
+            enabled: true,
             description: 'Displays queue',
             type: 'music',
             command: 'library/music/queue.js',
             args: ''
         },
         resume: {
-            enabled: false,
+            enabled: true,
             description: 'Resumes current song',
             type: 'music',
             command: 'library/music/resume.js',
             args: ''
         },
         volume: {
-            enabled: false,
+            enabled: true,
             description: 'Sets the current volume',
             type: 'music',
             command: 'library/music/volume.js',
