5e72bc3b5d2b421bcece8cacedb97f391cf74510
SharkFin <sharkfinpro@hotmail.com>
2019-01-04 18:56:05 -0800

    update

--- source/commands/library/general/login.js
+++ source/commands/library/general/login.js
@@ -1,12 +1,12 @@
 "use strict";
 const Discord = require('discord.js');
 module.exports = (message, commandList, config, server) => {
-      /*server.oauthClient.requestToken()
+      server.oauthClient.requestToken()
         .then(response => {
             message.channel.send('Sending you instructions in DM!')
             message.author.send(new Discord.MessageEmbed()
                 .setColor(0x20B2AA)
-                .setTitle('Click the link below to connect your Khan Academy and Discord accounts.')
+                .setTitle('Click the link below to link your Khan Academy and Discord accounts.')
                 .setDescription(`[Connect Khan Academy Account](https://www.khanacademy.org/api/auth2/authorize?oauth_token=${response.token})`));
-      })*/
+      })
 };
